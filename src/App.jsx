b2e0d0221760dee6f1b97dbb9de1b58ee366f5ee
mohamadezzat-sudo <mohamadezzat@gmail.com>
2026-08-12 00:02:24 +0300

    Add songs tutorial page and route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Welcome from './pages/Welcome';
+import SongsPage from './pages/SongsPage';
 import ProtectedLayout from './layouts/ProtectedLayout';
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Welcome />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/songs" element={<SongsPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
