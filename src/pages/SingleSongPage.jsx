713c72cd4aec9e0836775aa1f096412474c76738
Ron-sagiv <pedrozeeg@gmail.com>
2026-08-14 17:43:47 +0200

    Added backing track component

--- src/pages/SingleSongPage.jsx
+++ src/pages/SingleSongPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 import { isSongSaved, saveSong, removeSavedSong } from '../utils/savedSongs';
 import LessonsCarousel from '../components/LessonsCarousel';
 import OriginalVideo from '../components/OriginalVideo';
+import BackingTrack from '../components/BackingTrack';
 
 // Same mapping as Home / SearchBar.
 const DIFFICULTY_BADGE = {
@@ -83,6 +84,7 @@ const SingleSongPage = () => {
             </header>
             <LessonsCarousel song={song} />
             <OriginalVideo song={song} />
+            <BackingTrack song={song} />
           </>
         )}
       </div>
